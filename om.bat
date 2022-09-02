16d07eb572239c7bfcd51a535cefd940a4aafe86
Nickola-009 <112381077+Nickola-009@users.noreply.github.com>
2022-09-02 13:06:14 +0530

    Delete om.bat

--- om.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\Nikhill\eclipse-workspace\TestAutomation
-cd %projectLocation%
-set classpath=C:\Users\Nikhill\eclipse-workspace\TestAutomation\lib\*
-java org.testng.TestNG testng.xml
-pause
