f665af002f49be1e35630f482b8a3346824d1de0
Nickola-009 <112381077+Nickola-009@users.noreply.github.com>
2022-09-02 13:06:30 +0530

    Delete techno.bat

--- techno.bat
+++
@@ -1,6 +0,0 @@
-set ProjectPath=C:\Users\Nikhill\eclipse-workspace\TestAutomation
-echo %ProjectPath%
-set classpath=C:\Users\Nikhill\eclipse-workspace\TestAutomation\.classpath
-echo %classpath%
-java org.testng.TestNG testng.xml
-pause
